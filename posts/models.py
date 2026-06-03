da1728081e992917d880a0196effa0450661bf58
ashenafi <ashenafiadola05@gmail.com>
2026-06-03 17:09:18 +0300

    defined post db model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='posts/images/', null=True, blank=True)
+    description = models.TextField()
+    posted_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    
